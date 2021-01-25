9e868800f92e63e0575fafe56d45a2b7affb3792
KarinaLarionova <karina.davytova@gmail.com>
2021-01-25 19:58:01 +0300

    reloading

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
 
+    //nav-page
+    $('#page-nav').onePageNav({
+        currentClass: 'active',
+        changeHash: false,
+        scrollSpeed: 750,
+        scrollThreshold: 0.5,
+        filter: '',
+        easing: 'swing',
+        begin: function () {},
+        end: function () {},
+        scrollChange: function ($currentListItem) {}
+    });
+
     const menuToggle = document.querySelector('.toggle-menu');
     const mobMenu = document.querySelector ('.header-menu');
     const overlayEl = document.querySelector ('#overlay');
@@ -51,12 +64,101 @@ $(document).ready(function () {
         
     });
 
+//фильтрация проектов
+let containerEl = document.querySelector('#portfolio-projects');
+let mixer = mixitup(containerEl, {
+    classNames: {
+        block: ""
+    }
+});
+
+//форма и плейсхолдер
+const formInputs = document.querySelectorAll('.form-field');
+for(let item of formInputs){
+    const thisPlaceholder = item.nextElementSibling;
+
+    item.addEventListener('focus', function(){
+        thisPlaceholder.classList.add('active');
+    });
+    item.addEventListener('blur', function() {
+        if(item.value==''){
+            thisPlaceholder.classList.remove('active');
+        }
+    });
+}
+
+//валидация формы
+$('#contact-form').validate({
+    rules: {
+        email: {
+            required: true,
+            email: true
+        },
+        theme: {
+            required: true
+        },
+        message: {
+            required: true
+        }
+    },
+    messages: {
+        email: {
+            required: 'Введите email',
+            email: 'отсутсвует символ @'
+        },
+        theme: {
+            required: 'Введите тему сообщения'
+        },
+        message: {
+            required: 'Введите текстсообщения'
+        }
+    },
+    submitHandler: function (form) {
+        ajaxFormSubmit();
+    }
+
+})
+
+
+// Функция AJAX запрса на сервер
+
+function ajaxFormSubmit() {
+
+    let string = $("#contact-form").serialize(); // Соханяем данные введенные в форму в строку.
+
+    //Формируем ajax запрос
+    $.ajax({
+        type: "POST", // Тип запроса - POST
+        url: "php/mail.php", // Куда отправляем запрос
+        data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+
+        // Функция если все прошло успешно
+        success: function (html) {
+            $("#contact-form").slideUp(800);
+            $('#answer').html(html);
+        }
+    });
+
+    // Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+    return false;
+}
+
+//backTop button
+$('#backTop').hide();
+
+$(window).scroll(function () {
+
+    if($(this).scrollTop() > 200){
+        $('#backTop').fadeIn();
+    }
+    else{
+        $('#backTop').fadeOut();
+    }
+})
+
 
 
 
 
 
 });
-/*document.querySelector('.toggle-menu').onclick = function(){
-    this.classList.toggle('active');
-}*/
